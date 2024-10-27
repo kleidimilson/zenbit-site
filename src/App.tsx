4c8034860c130b8af8b080380a15c22bf9302d14
kleidimilson <kleidimilson@gmail.com>
2024-10-27 19:03:32 -0300

    Adicionando slide de serviços

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,77 @@ import { ScrollToTop } from './components/ScrollToTop';
 
 import './App.css';
 
+import start from './assets/66a2608272256f8a1ab4f272_Star.svg';
+
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <div className="flex items-center bg-[#5BC560] h-[80px] text-xl	 text-black font-bold relative overflow-hidden whitespace-nowrap">
+        <div className="animate-marquee flex gap-[100px]">
+          <div className="flex items-center">
+            <img src={start} />
+            <span className="mx-2">Aplicativos Web</span>
+          </div>
+          <div className="flex items-center ">
+            <img src={start} />
+            <span className="mx-2">Desenvolvimento Mobile</span>
+          </div>
+          <div className="flex items-center">
+            <img src={start} />
+            <span className="mx-2">Consultoria TI</span>
+          </div>
+          <div className="flex items-center">
+            <img src={start} />
+            <span className="mx-2">Especialistas</span>
+          </div>
+          <div className="flex items-center">
+            <img src={start} />
+            <span className="mx-2">UI/UX</span>
+          </div>
+          <div className="flex items-center">
+            <img src={start} />
+            <span className="mx-2">Aplicativos Web</span>
+          </div>
+          <div className="flex items-center ">
+            <img src={start} />
+            <span className="mx-2">Desenvolvimento Mobile</span>
+          </div>
+          <div className="flex items-center">
+            <img src={start} />
+            <span className="mx-2">Consultoria TI</span>
+          </div>
+          <div className="flex items-center">
+            <img src={start} />
+            <span className="mx-2">Especialistas</span>
+          </div>
+          <div className="flex items-center">
+            <img src={start} />
+            <span className="mx-2">UI/UX</span>
+          </div>
+          <div className="flex items-center">
+            <img src={start} />
+            <span className="mx-2">Aplicativos Web</span>
+          </div>
+          <div className="flex items-center ">
+            <img src={start} />
+            <span className="mx-2">Desenvolvimento Mobile</span>
+          </div>
+          <div className="flex items-center">
+            <img src={start} />
+            <span className="mx-2">Consultoria TI</span>
+          </div>
+          <div className="flex items-center">
+            <img src={start} />
+            <span className="mx-2">Especialistas</span>
+          </div>
+          <div className="flex items-center">
+            <img src={start} />
+            <span className="mx-2">UI/UX</span>
+          </div>
+        </div>
+      </div>
 
       <About />
       <HowItWorks />
